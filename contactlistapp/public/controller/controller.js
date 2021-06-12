b01555749d7617959cee884b1dcd36dcc7c4fecd
Shihao Sun <shihao.m.sun@gmail.com>
2021-06-12 18:29:31 -0400

    Day 10

--- contactlistapp/public/controller/controller.js
+++ contactlistapp/public/controller/controller.js
@@ -3,13 +3,6 @@
 function AppCtrl($scope, $http){
     console.log("Hello from Controller");
 
-    // $http is a service, is not a module; 可以去Angluar 官网上搜 $http
-    // 这里没有button，而是 controller makes a directs call to express
-    $http.get('/contactlist').success(function(response) {                // 这里的 '/contactlist' 其实是一个 url
-        console.log("I got the data", response)
-        $scope.contactlist = response;
-    })
-
     // person1 = {
     //     name: "John",
     //     email: "john@gmail.com",
@@ -30,4 +23,50 @@ function AppCtrl($scope, $http){
 
     // // send it across the view
     // $scope.contactlist = contactlist;
+
+
+    var refresh = function(){
+        // $http is a service, is not a module; 可以去Angluar 官网上搜 $http
+        // 这里没有 button，而是 controller makes a directs call to express
+        $http.get('/contactlist').success(function(response) {                // 这里的 '/contactlist' 其实是一个 url
+            // console.log("I got the data", response)
+            $scope.contactlist = response;
+            $scope.contact = ""              // 需要这行，因为想让 refresh 后 input 框中的 input text 消失
+        });
+    }
+
+    refresh();
+
+    $scope.addContact = function(){
+        console.log($scope.contact)         // 这里的contact object 会有 name, email, number
+
+        // console.log($scope.contact instanceof Object)       // true
+        // console.log($scope.contact instanceof String)       // false
+
+        $http.post('./contactlist', $scope.contact).success(function(response){
+            console.log(response)    // it actually comes back with an ID, meaning it is inserted into the database
+            refresh()
+        })
+    }
+
+    $scope.remove = function(id){
+        console.log(id)      // 基于 id 从 database 中删除
+        $http.delete('/contactlist/' + id).success(function(response){
+            refresh();       // delete 成功后更新界面
+        })
+    }
+
+    $scope.edit = function(id){
+        console.log(id)
+        $http.get('/contactlist/' + id).success(function(response){
+            // put it in the scope so that is can get displayed in the input box
+            $scope.contact = response;
+        })
+    }
+
+    $scope.update = function(){
+        console.log($scope.contact._id);
+        // 这里的 $scope.contact 是 update 后的 new data
+        $http.put('/contactlist/' + $scope.contact._id, $scope.contact)
+    }
 }
--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -1,32 +1,95 @@
-var express = require('express')
-var app = express()      // invoke the construction and create an instance. This app mean s the entire web application
+var express = require('express')      // here we're using Express server. Express is a server for node js. If you want to use this package is JS file, you need to require it.
+var app = express()      // invoke the construction and create an instance. This app means the entire web application
+var mongojs = require('mongojs')
+var db = mongojs('contactlist', ['contactlist'])       // need to specify the (database name, [connection name 1, connection name 2, ...]) here
 
+app.use(express.json());
 app.use(express.static(__dirname + '/public'));      // let our server knows to go to the public directory
 
 app.get('/contactlist', function(req, res){
     // res.send("received req from controller")
     console.log("received req from controller")
 
-    person1 = {
-        name: "John",
-        email: "john@gmail.com",
-        number: "(111) 111-1111"
-    };
-    person2 = {
-        name: "Jim",
-        email: "jim@gmail.com",
-        number: "(222) 222-2222"
-    };
-    person3 = {
-        name: "Jill",
-        email: "jill@gmail.com",
-        number: "(333) 333-3333"
-    };
+    // Comment out the data, because we are getting it from the database
+    // person1 = {
+    //     name: "John",
+    //     email: "john@gmail.com",
+    //     number: "(111) 111-1111"
+    // };
+    // person2 = {
+    //     name: "Jim",
+    //     email: "jim@gmail.com",
+    //     number: "(222) 222-2222"
+    // };
+    // person3 = {
+    //     name: "Jill",
+    //     email: "jill@gmail.com",
+    //     number: "(333) 333-3333"
+    // };
+    // var contactlist = [person1, person2, person3]
+    // res.json(contactlist);
 
-    var contactlist = [person1, person2, person3]
-    res.json(contactlist);
+    // Get data from database. Here, we have a callback function.
+    // Whenever find complete, this function will be run; Either we'll have an err or we'll have documents
+    // So either err is null or docs is null
+    db.contactlist.find(function(err, docs){
+        // console.log(docs)
+        res.json(docs)
+    })
+    // find: function, this function is acutally converted to Mongo DB query and get executed in the database.
+    // db.(collection name).(function)
+
 })
 
+
+app.post('/contactlist', function(req, res){
+    console.log(req.body)       // 如果不加 parser undefind, beacause we need a parser to read the date in the body
+
+    // the latest Express has a body parser, 也可以自己安装别的 body parser
+    
+    // console.log(req.body instanceof Object)           // true
+    // console.log(req.body instanceof String)           // false
+
+    // insert the data into the database
+    // if insert is successful, mongojs will return us the inserted data (docs)
+    db.contactlist.insert(req.body, function(err, docs){
+        // once the insertion is successful, we will send the inserted the data back to the controller for display
+        res.json(docs)
+    })
+    
+})
+
+
+app.delete('/contactlist/:id', function(req, res){
+    // delete needs to retrieve the id from the params
+    var id = req.params.id
+    db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);             // send back the deleted document
+    })
+
+})
+
+
+app.get('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    });
+})
+
+
+app.put('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+                                  update: {$set: {name:req.body.name, email:req.body.email, number: req.body.number}},
+                                  new: true}, 
+                                  function(err, doc){
+                                        res.json(doc);
+                                  })
+});
+
+
+// 让这个 app 跑在 9000 端口
 app.listen(9000, function(){
     console.log("Server running @9000")
 })
