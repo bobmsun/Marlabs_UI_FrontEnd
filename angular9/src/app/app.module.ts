008836aef84696d7573bfc071df96c21249495ef
Shihao Sun <shihao.m.sun@gmail.com>
2021-06-20 21:17:23 -0400

    angular9

--- angular9/src/app/app.module.ts
+++ angular9/src/app/app.module.ts
@@ -7,20 +7,31 @@ import { FirstComponent } from './first/first.component';
 import { ClassBindingComponent } from './class-binding/class-binding.component';
 import { ThirdComponent } from './third/third.component';
 import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './child/child.component';
+import { ContentprojectionComponent } from './contentprojection/contentprojection.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeService } from './employee.service';
+import { HttpClientModule  } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
     ClassBindingComponent,
-    ThirdComponent
+    ThirdComponent,
+    ChildComponent,
+    ContentprojectionComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
